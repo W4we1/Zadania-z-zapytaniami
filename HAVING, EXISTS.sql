699e6df3ee669a497e97d4d17d5fb0ab1b0fc017
W4we1 <Wawel2004@gmail.com>
2023-01-04 08:39:59 +0100

    Praca w szkole

--- HAVING, EXISTS.sql
+++ HAVING, EXISTS.sql
@@ -1,10 +1,10 @@
 1.
 
-SELECT coutry FROM customers HAVING COUNT (id) > 5;
+SELECT coutry FROM customers HAVING COUNT (id)>5;
 
 2.
 
-
+SELECT customers FROM customers JOIN orders on customers.id=orders.customer_id HAVING COUNT (orders.id)>2
 
 3.
 
