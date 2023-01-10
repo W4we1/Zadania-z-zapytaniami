9e72c1f6d3cca5ed18e49bb9afd526240b5476cd
W4we1 <Wawel2004@gmail.com>
2023-01-10 15:57:06 +0100

    Praca w domu

--- HAVING, EXISTS.sql
+++ HAVING, EXISTS.sql
@@ -4,11 +4,11 @@ SELECT coutry FROM customers HAVING COUNT (id)>5;
 
 2.
 
-SELECT customers FROM customers JOIN orders on customers.id=orders.customer_id HAVING COUNT (orders.id)>2
+SELECT * FROM customers JOIN orders on customers.id=orders.customer_id HAVING COUNT (orders.id)>2 ORDER BY customers.id DESC;
 
 3.
 
-
+SELECT * FROM employees JOIN orders on orders.id=orders.employee_id HAVING COUNT (orders.id)<10 ORDER BY employee.id DESC;
 
 4.
 
